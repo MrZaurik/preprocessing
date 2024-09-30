5149de29a2185c12241da75584babb80f450d04d
Angelo <abrittojose@gmail.com>
2024-09-29 22:21:29 -0500

    Create bib to df function

---
+++ src/modules/scopus_df.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import bibtexparser
+
+def bib_to_df(file_path):
+    
+    try:
+        with open(file_path, 'r', encoding='utf-8') as bibfile:
+            bibtex_str = bibfile.read()
+        
+        library = bibtexparser.loads(bibtex_str)
+
+        entries_data = []
+
+        for entry in library.entries:
+            entry_data = {}
+            for i in entry:
+                entry_data[i] = entry.get(i, '')
+            entries_data.append(entry_data)
+
+        return pd.DataFrame(entries_data)
+    
+    except FileNotFoundError:
+        print(f"El archivo {file_path} no se encuentra.")
+
+    except Exception as e:
+        print(f"Ha ocurrido el error: {e}")
+
+    return None
